843909bec16a32864c0a9248c29a2d985bf71e07
Noah Spahn <noah-de@users.noreply.github.com>
2022-09-27 21:17:59 -0700

    new graph size

--- swel.py
+++ swel.py
@@ -75,9 +75,10 @@ Pf = 1./fmid[0, :]                            # convert from freq. to period
 
 # plotting options
 fig, ax = plt.subplots()
-
-
+fig.set_size_inches(5, 3)
 #fig.set_size_inches(11, 5)
+
+
 #fig.savefig('SWH.png', dpi=100)
 
 for idx, _ in enumerate(pmid):               # loop over period mid-point
